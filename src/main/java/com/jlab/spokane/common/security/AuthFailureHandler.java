b218fd90b6bf5119edb9d0299d82f6d6c4d8c9af
jjiy01 <jjiy01@gmail.com>
2017-06-26 23:39:30 +0900

    initial commit

---
+++ src/main/java/com/jlab/spokane/common/security/AuthFailureHandler.java
@@ -0,0 +1,23 @@
+package com.jlab.spokane.common.security;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+public class AuthFailureHandler implements AuthenticationFailureHandler {
+
+	@Override
+	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException exception) throws IOException, ServletException {
+		response.setContentType("application/json");
+		PrintWriter pw = response.getWriter();
+		pw.write(String.format("{ \"status\": \"fail\", \"result\" : { \"%s\" }}", exception.getMessage()));
+	}
+
+}
---
+++ src/main/java/com/jlab/spokane/common/security/RestAuthenticationEntryPoint.java
@@ -0,0 +1,8 @@
+package com.jlab.spokane.common.security;
+
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
+
+public class RestAuthenticationEntryPoint {
+
+}
